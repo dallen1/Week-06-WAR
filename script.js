a074776231ce115565624a3d4dfaeeae2a438b43
dallen1 <dallen1@epscor.unm.edu>
2023-05-02 08:34:40 -0600

    class creation

---
+++ script.js
@@ -0,0 +1,27 @@
+class Card {
+    constructor(){
+        this.house = [];
+        this.value = [];
+    }
+
+}
+
+class Player {
+    constructor(){
+        this.name = '';
+        this.score = 0;
+    }
+
+}
+
+class Deck {
+    constructor(card){
+        this.card = card;
+        this.deck = [];
+    }
+
+}
+
+class Game {
+
+}
